7d278dfbdaf1146351c9875a69170e7d23af2595
JAlejandroQuevedo <alejandro_ya23@hotmail.com>
2024-04-08 12:49:17 -0700

    Commit de trabajo

---
+++ src/App/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { NavBarContainer } from '../components/layout/NavBar/NavBarContainer'
+
+export const App = () => {
+  return (
+    <NavBarContainer />
+  )
+}
+
---
+++ src/components/layout/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <div>
+            <h1 className='red'>Milicita te quierooooooooooooo</h1>
+        </div>
+    )
+}
